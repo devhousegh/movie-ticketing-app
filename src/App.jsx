2013bf50edb4b010658e07249da15308e8c410e0
Nana K <73991180+topboyasante@users.noreply.github.com>
2023-03-12 19:40:16 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Homepage from './pages/homepage'
 import SignIn from './pages/authentication/SignIn'
 import SignUp from './pages/authentication/SignUp'
+import { AppContext } from './context/AppContext'
+import axios from 'axios'
 
 function App() {
+  //Global State
+  const [movies,setMovies] = useState([])
+
+     const headers={
+         'Authorization':
+         'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NzU3YTdjMDNiN2Q2YzJiYmI4OWMyYzljODZiMjcwZSIsInN1YiI6IjY0MGUyNTFhZWRlMWIwMDBkOTc1NDE5OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gizTyQELHGPKTtCxdceD-yoWMF_RUQVcFrqjYcrYa1k'
+     }
+
+  useEffect(()=>{
+    async function fetchDB(){
+      await axios.get('https://api.themoviedb.org/3/movie/now_playing',{headers})
+      .then((res)=>{
+          setMovies(res.data.results)
+      })
+    }
+    fetchDB()
+  })
+
   return (
-    <main>
-      <Routes>
-        <Route path='/' element={<Homepage/>}/>
-        <Route path='sign-in' element={<SignIn/>}/>
-        <Route path='sign-up' element={<SignUp/>}/>
-      </Routes>
-    </main>
+      <AppContext.Provider value={movies}>
+        <main>
+          <Routes>
+            <Route path='/' element={<Homepage/>}/>
+            <Route path='sign-in' element={<SignIn/>}/>
+            <Route path='sign-up' element={<SignUp/>}/>
+          </Routes>
+        </main>
+      </AppContext.Provider>
   )
 }
 
