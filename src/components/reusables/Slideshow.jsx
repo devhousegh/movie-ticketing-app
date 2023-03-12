20fbc3833abf27a82645a2de728a7e762962cc87
Nana K <73991180+topboyasante@users.noreply.github.com>
2023-03-12 20:23:04 +0000

    Slideshow complete

---
+++ src/components/reusables/Slideshow.jsx
@@ -0,0 +1,51 @@
+import React, { useContext } from 'react'
+import { AppContext } from '../../context/AppContext'
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-fade";
+
+// import required modules
+import { Autoplay,EffectFade} from "swiper";
+
+function Slideshow({children}) {
+  const movies = useContext(AppContext)
+  return (
+    <main>
+        <Swiper
+        spaceBetween={30}
+        effect={"fade"}
+        navigation={true}
+        pagination={{
+          clickable: true,
+        }}
+        autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+        }}
+        modules={[Autoplay,EffectFade]}
+        className="w-full h-full"
+        >
+                {movies.map(movie=>{
+                    return(
+                        <SwiperSlide key={movie.id} className='relative'>
+
+                           <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} 
+                            alt={movie.original_title}
+                            className='w-full h-full'/>
+
+                            <div className="absolute z-[10] top-0 left-0 bg-black w-full h-full opacity-70"/>
+                                {children}
+                            
+                        </SwiperSlide>
+                    )
+                })}
+        </Swiper>
+    </main>
+  )
+}
+
+export default Slideshow
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Slideshow from '../components/reusables/Slideshow'
 
 function Homepage() {
   return (
-    <div>Homepage</div>
+    <main>
+      <Slideshow>
+        <div className="z-[20] absolute top-0 left-0 text-white p-5">
+          h1elooj
+        </div>
+      </Slideshow>
+    </main>
   )
 }
 
