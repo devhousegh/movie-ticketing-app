8cc35eaba729928b6c4c741237e1384cddcd04d3
Nana K <73991180+topboyasante@users.noreply.github.com>
2023-03-12 18:48:32 +0000

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Homepage from './pages/homepage'
+import SignIn from './pages/authentication/SignIn'
+import SignUp from './pages/authentication/SignUp'
 
 function App() {
   return (
-    <div>App</div>
+    <main>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='sign-in' element={<SignIn/>}/>
+        <Route path='sign-up' element={<SignUp/>}/>
+      </Routes>
+    </main>
   )
 }
 
